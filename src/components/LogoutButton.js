15d26ef8e862ab72142725cc15b9e5c5cdfc7e0d
Demis Previotto <demis.previotto@gmail.com>
2023-05-09 15:44:00 +0200

    .gitignore update

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -4,7 +4,9 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LogoutButton = () => {
     const { logout } = useAuth0();
     return (
-        <button onClick={() => logout({ returnTo: window.location.origin })}>Logout</button>
+        <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}
+        >Logout
+        </button>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -7,14 +7,18 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
+const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
+const scope = process.env.REACT_APP_AUTH0_SCOPE
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    authorizationParams={{
-      redirect_uri: window.location.origin}}
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: { audience },
+        scope: { scope }
+      }}
     >
       <AppProvider>
         <App />
@@ -24,7 +28,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
